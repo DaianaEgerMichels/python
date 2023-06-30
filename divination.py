7e4659cddac50248c8c92cdd4b6c14605923a2dc
Daiana Michels <daianaem.12@gmail.com>
2023-06-29 21:43:25 -0300

    adds some validations

--- divination.py
+++ divination.py
@@ -10,7 +10,17 @@ print("You typed: ", kick)
 
 kick = int(kick)
 
-if secret_number == kick:
+hit = secret_number == kick
+bigger = kick > secret_number
+smaller = kick < secret_number
+
+if (hit):
     print("You're right :)")
 else:
     print("You're wrong :(")
+    if(bigger):
+        print("Your kick was bigger than the secret number.")
+    elif(smaller):
+        print("Your kick was smaller than the secret number.")
+
+print("End of the game")
