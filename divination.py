4533df818c16949e6a984f62a21c886a90df870e
Daiana Michels <daianaem.12@gmail.com>
2023-07-03 21:05:10 -0300

    implements repeating structure

--- divination.py
+++ divination.py
@@ -3,24 +3,28 @@ print("Welcome to the divination game!")
 print("*******************************")
 
 secret_number = 47
+total_attempts = 3
+round = 1
 
-kick = input("Enter a number: ")
+while (round <= total_attempts):
+    print("Attempts:", round, "of", total_attempts)
+    kick = input("Enter a number: ")
+    print("You typed: ", kick)
 
-print("You typed: ", kick)
+    kick = int(kick)
 
-kick = int(kick)
+    hit = secret_number == kick
+    bigger = kick > secret_number
+    smaller = kick < secret_number
 
-hit = secret_number == kick
-bigger = kick > secret_number
-smaller = kick < secret_number
+    if (hit):
+        print("You're right :)")
+    else:
+        print("You're wrong :(")
+        if(bigger):
+            print("Your kick was bigger than the secret number.")
+        elif(smaller):
+            print("Your kick was smaller than the secret number.")
 
-if (hit):
-    print("You're right :)")
-else:
-    print("You're wrong :(")
-    if(bigger):
-        print("Your kick was bigger than the secret number.")
-    elif(smaller):
-        print("Your kick was smaller than the secret number.")
+    round += 1
 
-print("End of the game")
