fbd24f1cd8abd19c6d001a62b0844242d8383b82
Daiana Michels <daianaem.12@gmail.com>
2023-07-04 21:13:48 -0300

    adds for loop

--- divination.py
+++ divination.py
@@ -28,3 +28,24 @@ while (round <= total_attempts):
 
     round += 1
 
+for round in range(1, total_attempts + 1):
+    print("Attempts: {} of {}".format(round, total_attempts))
+    kick = input("Enter a number: ")
+    print("You typed: ", kick)
+
+    kick = int(kick)
+
+    hit = secret_number == kick
+    bigger = kick > secret_number
+    smaller = kick < secret_number
+
+    if (hit):
+        print("You're right :)")
+    else:
+        print("You're wrong :(")
+        if(bigger):
+            print("Your kick was bigger than the secret number.")
+        elif(smaller):
+            print("Your kick was smaller than the secret number.")
+
+print("End the game...")
