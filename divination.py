7cfbc49ff2ca0a34ad6655d380bce254b1ffad58
Daiana Michels <daianaem.12@gmail.com>
2023-07-04 21:34:52 -0300

    adds break and continue

--- divination.py
+++ divination.py
@@ -6,34 +6,38 @@ secret_number = 47
 total_attempts = 3
 round = 1
 
-while (round <= total_attempts):
-    print("Attempts: {} of {}".format(round, total_attempts))
-    kick = input("Enter a number: ")
-    print("You typed: ", kick)
-
-    kick = int(kick)
-
-    hit = secret_number == kick
-    bigger = kick > secret_number
-    smaller = kick < secret_number
-
-    if (hit):
-        print("You're right :)")
-    else:
-        print("You're wrong :(")
-        if(bigger):
-            print("Your kick was bigger than the secret number.")
-        elif(smaller):
-            print("Your kick was smaller than the secret number.")
-
-    round += 1
+# while (round <= total_attempts):
+#     print("Attempts: {} of {}".format(round, total_attempts))
+#     kick = input("Enter a number: ")
+#     print("You typed: ", kick)
+#
+#     kick = int(kick)
+#
+#     hit = secret_number == kick
+#     bigger = kick > secret_number
+#     smaller = kick < secret_number
+#
+#     if (hit):
+#         print("You're right :)")
+#     else:
+#         print("You're wrong :(")
+#         if(bigger):
+#             print("Your kick was bigger than the secret number.")
+#         elif(smaller):
+#             print("Your kick was smaller than the secret number.")
+#
+#     round += 1
 
 for round in range(1, total_attempts + 1):
     print("Attempts: {} of {}".format(round, total_attempts))
-    kick = input("Enter a number: ")
+    kick = input("Enter a number between 1 and 100: ")
     print("You typed: ", kick)
 
     kick = int(kick)
+
+    if (kick < 1 or kick > 100):
+        print("You must enter a number between 1 and 100!")
+        continue
 
     hit = secret_number == kick
     bigger = kick > secret_number
@@ -41,6 +45,7 @@ for round in range(1, total_attempts + 1):
 
     if (hit):
         print("You're right :)")
+        break
     else:
         print("You're wrong :(")
         if(bigger):
