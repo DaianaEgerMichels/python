bc698aef470be0b0089d8664ac1d0b19e701d457
Daiana Michels <daianaem.12@gmail.com>
2023-07-05 21:40:14 -0300

    implements random secret number generation

--- divination.py
+++ divination.py
@@ -1,13 +1,15 @@
+import random
+
 print("*******************************")
 print("Welcome to the divination game!")
 print("*******************************")
 
-secret_number = 47
+secret_number = random.randrange(1, 101)
 total_attempts = 3
-round = 1
-
-# while (round <= total_attempts):
-#     print("Attempts: {} of {}".format(round, total_attempts))
+rounded = 1
+print(secret_number)
+# while (rounded <= total_attempts):
+#     print("Attempts: {} of {}".format(rounded, total_attempts))
 #     kick = input("Enter a number: ")
 #     print("You typed: ", kick)
 #
@@ -26,10 +28,10 @@ round = 1
 #         elif(smaller):
 #             print("Your kick was smaller than the secret number.")
 #
-#     round += 1
+#     rounded += 1
 
-for round in range(1, total_attempts + 1):
-    print("Attempts: {} of {}".format(round, total_attempts))
+for rounded in range(1, total_attempts + 1):
+    print("Attempts: {} of {}".format(rounded, total_attempts))
     kick = input("Enter a number between 1 and 100: ")
     print("You typed: ", kick)
 
