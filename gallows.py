229b081f88d05f0b75a509f3eb203d8cb16de1a5
Daiana Michels <daianaem.12@gmail.com>
2023-07-10 21:36:01 -0300

    starts construction of the gallows game

--- gallows.py
+++ gallows.py
@@ -3,6 +3,23 @@ def play():
     print("Welcome to the gallows game!")
     print("*******************************")
 
+    secret_word = "flower"
+
+    right = False
+    hanged = False
+
+    while(not hanged and not right):
+
+        kick = input("Say a letter: ")
+        index = 0
+
+        for letter in secret_word:
+            if(kick == letter):
+                print("Found the letter {} in position {}".format(letter, index))
+            index = index + 1
+
+        print("playing...")
+
     print("End the game...")
 
 if (__name__ == "__main__"):
