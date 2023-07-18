719fa2031ac905de20f087ce9273579fb6f1a809
Daiana Michels <daianaem.12@gmail.com>
2023-07-17 21:42:10 -0300

    adds number of allowed mistakes

--- gallows.py
+++ gallows.py
@@ -3,29 +3,40 @@ def play():
     print("Welcome to the gallows game!")
     print("*******************************")
 
-    secret_word = "flower"
+    secret_word = "flower".upper()
     right_letters = ["_", "_", "_", "_", "_", "_"]
 
     right = False
     hanged = False
+    mistakes = 0
 
     print(right_letters)
 
     while(not hanged and not right):
 
         kick = input("Say a letter: ")
-        kick = kick.strip()
-        index = 0
+        kick = kick.strip().upper()
 
-        for letter in secret_word:
-            if(kick.lower() == letter.lower()):
-                print("Found the letter {} in position {}".format(letter, index))
-                right_letters[index] = letter
-                print(right_letters)
-            index = index + 1
 
-        print("playing...")
+        if(kick in secret_word):
+            index = 0
+            for letter in secret_word:
+                if(kick == letter):
+                    print("Found the letter {} in position {}".format(letter, index))
+                    right_letters[index] = letter
+                    print(right_letters)
+                index += 1
+        else:
+            mistakes += 1
 
+        hanged = mistakes == 6
+        right = "_" not in right_letters
+        print(right_letters)
+
+    if(right):
+        print("You won :)")
+    else:
+        print("You lost :(")
     print("End the game...")
 
 if (__name__ == "__main__"):
