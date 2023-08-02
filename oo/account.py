30f0454c5e1c74b0583c76a5bea9c8272d765034
Daiana Michels <daianaem.12@gmail.com>
2023-08-01 21:08:27 -0300

    adds methods into class

--- oo/account.py
+++ oo/account.py
@@ -7,3 +7,10 @@ class Account:
         self.balance = balance
         self.limit = limit
 
+
+    def generate_extract(self):
+        print("The balance of this account is {}, owner {}".format(self.balance, self.owner))
+    def to_deposit(self, value):
+        self.balance += value
+    def to_withdraw(self, value):
+        self.balance -= value
