3829751f51322c0c763e76035730054a8954825b
Daiana Michels <daianaem.12@gmail.com>
2023-08-01 21:25:36 -0300

    adds Dates class

---
+++ oo/dates.py
@@ -0,0 +1,10 @@
+class Dates:
+
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month = month
+        self.year = year
+
+
+    def date_format(self):
+        print("{:02d}/{:02d}/{}".format(self.day, self.month, self.year))
