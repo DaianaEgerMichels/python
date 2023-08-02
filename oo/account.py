8b7b5262a7914d976f9095b6326adaa1c2601654
Daiana Michels <daianaem.12@gmail.com>
2023-08-02 20:47:10 -0300

    modifies access to attributes and adds the method to transfer

--- oo/account.py
+++ oo/account.py
@@ -2,15 +2,20 @@ class Account:
 
     def __init__(self, number, owner, balance, limit):
         print("Building the object... {}".format(self))
-        self.number = number
-        self.owner = owner
-        self.balance = balance
-        self.limit = limit
+        self.__number = number
+        self.__owner = owner
+        self.__balance = balance
+        self.__limit = limit
 
 
     def generate_extract(self):
-        print("The balance of this account is {}, owner {}".format(self.balance, self.owner))
+        print("The balance of this account is {}, owner {}".format(self.__balance, self.__owner))
     def to_deposit(self, value):
-        self.balance += value
+        self.__balance += value
     def to_withdraw(self, value):
-        self.balance -= value
+        self.__balance -= value
+
+    def to_transfer(self, value, destiny):
+        self.to_withdraw(value)
+        destiny.to_deposit(value)
+
